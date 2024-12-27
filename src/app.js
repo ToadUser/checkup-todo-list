0f974a70e54a646b0c50cf650f4c3d22ccf05d21
Ivan <ivanturchinsh@gmail.com>
2024-12-27 18:52:43 +0300

    реализована стилизация страницы

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
-alert('работает-не трогай')
+const btnLogin = document.getElementById('btnLogin');
+const btnReg = document.getElementById('reg');
+const btnFogot = document.getElementById('fogot');
+const disLogin = document.getElementById('login');
+const disReg = document.getElementById('register');
+const disFgt = document.getElementById('fgt');
+const btnLoginFgt = document.getElementById('btnLoginFgt');
+const btnRegFgt = document.getElementById('regFgt');
+const btnFogotReg = document.getElementById('fogotReg');
+
+btnLogin.addEventListener('click', Login);
+btnReg.addEventListener('click', Register);
+btnFogot.addEventListener('click', Fogot);
+btnFogotReg.addEventListener('click', Fogot);
+btnLoginFgt.addEventListener('click', Login);
+btnRegFgt.addEventListener('click', Register);
+
+function Login() {
+  disFgt.classList.add('hidden');
+  disLogin.classList.remove('hidden');
+  disReg.classList.add('hidden');
+}
+
+function Register() {
+  disFgt.classList.add('hidden');
+  disLogin.classList.add('hidden');
+  disReg.classList.remove('hidden');
+}
+
+function Fogot() {
+  disFgt.classList.remove('hidden');
+  disLogin.classList.add('hidden');
+  disReg.classList.add('hidden');
+}
